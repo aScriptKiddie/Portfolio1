e2675e569ff22642e71d3df4da1b1e61f09f0293
HackerBree <kagwe.bridgette@gmail.com>
2026-05-21 17:45:03 +1000

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> [PLEASE INSERT YOUR NAME] </strong>
+                <strong className="main-name"> [YOUR NAME] </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
